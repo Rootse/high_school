a19f4d929b6bda3d9e33cdd595bb513b42e0bb6d
yan.frolov <yan.frolov@osinit.com>
2023-07-26 21:09:26 +0300

    fix odometer

--- tasks/tasks_2_odometer.py
+++ tasks/tasks_2_odometer.py
@@ -1,8 +1,8 @@
 def odometer(a: list[int]) -> int:
     time, distance = 0, 0
     for i in zip(a[::2], a[1::2]):
-        t = i[1] - time
-        distance += i[0] * t
-        time += i[1]
+        time = i[1] - time
+        distance += i[0] * time
+        time = i[1]
 
     return distance
