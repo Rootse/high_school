20b681868a82a76870091e0be554be34bca1d4a4
yan.frolov <yan.frolov@osinit.com>
2023-08-05 21:40:59 +0300

    add task 11 big minus

---
+++ tasks/task_11_big_minus.py
@@ -0,0 +1,24 @@
+def BigMinus(s1: str, s2: str) -> str:
+    res = ""
+    carry = 0
+    for i in range(min(len(s1), len(s2))):
+        if ord(s1[i]) < ord(s2[i]):
+            s1, s2 = s2, s1
+
+    i, j = len(s1) - 1, len(s2) - 1
+    while i >= 0 or j >= 0:
+        x = int(s1[i]) if i >= 0 else 0
+        y = int(s2[j]) if j >= 0 else 0
+        z = x - y - carry
+
+        if z < 0:
+            z += 10
+            carry = 1
+        else:
+            carry = 0
+
+        res = str(z) + res
+        i -= 1
+        j -= 1
+
+    return res
