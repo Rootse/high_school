50ff1ee7e48584fdf25439dec7f67662db3e87f3
yan.frolov <yan.frolov@osinit.com>
2023-08-23 15:46:13 +0300

    edit task 22 SherlockValidString

--- tasks/task_22_sherlock_valid_string.py
+++ tasks/task_22_sherlock_valid_string.py
@@ -4,7 +4,9 @@ def SherlockValidString(s: str) -> bool:
         freq[letter] = freq.get(letter, 0) + 1
 
     values = list(freq.values())
-    print(values)
+    if len(values) == 2 and 1 in values:
+        return True
+
     return (max(values) == min(values) or
             (max(values) - 1 == min(values) and values.count(min(values)) == 1) or
             (min(values) + 1 == max(values) and values.count(max(values)) == 1))
