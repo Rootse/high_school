4c163fafae84926f194f4e8a2e22addba52843ee
yan.frolov <yan.frolov@osinit.com>
2023-08-26 09:14:39 +0300

    edit task 27 Football

---
+++ tasks/task_27_football.py
@@ -0,0 +1,19 @@
+def Football(f: list[int], n: int) -> bool:
+    if sorted(f) == f:
+        return True
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            f[i], f[j] = f[j], f[i]
+            if sorted(f) == f:
+                return True
+            f[i], f[j] = f[j], f[i]
+
+    for i in range(n):
+        for j in range(i + 2, n):
+            f[i:j + 1] = f[i:j + 1][::-1]
+            if sorted(f) == f:
+                return True
+            f[i:j + 1] = f[i:j + 1][::-1]
+
+    return False
