5c47ff1b69cfc1afef8c184b22be2ebe601144d5
yan.frolov <yan.frolov@osinit.com>
2023-09-09 17:57:36 +0300

    fix task 7

--- recursion/task_7_second_max.py
+++ recursion/task_7_second_max.py
@@ -1,14 +1,16 @@
-def second_max_rec(numbers: list[int], max_values: list[int]) -> int:
-    if len(numbers) == 0:
+def second_max_rec(numbers: list[int],  max_values: list, idx: int = 0) -> int:
+    if len(numbers) <= idx:
         return max_values[1]
 
-    if numbers[0] > max_values[0]:
-        max_values[1], max_values[0] = max_values[0], numbers[0]
-    elif numbers[0] > max_values[1]:
-        max_values[1] = numbers[0]
+    if numbers[idx] > max_values[0]:
+        max_values[1], max_values[0] = max_values[0], numbers[idx]
+    elif numbers[idx] > max_values[1]:
+        max_values[1] = numbers[idx]
 
-    return second_max_rec(numbers[1:], max_values)
+    return second_max_rec(numbers, max_values, idx + 1)
 
 
 def second_max(numbers: list[int]) -> int:
-    return second_max_rec(numbers, [numbers[0], numbers[0]])
+    if len(numbers) == 1:
+        return numbers[0]
+    return second_max_rec(numbers, [float('-inf'), float('-inf')])
--- recursion/tests/test_7_second_max.py
+++ recursion/tests/test_7_second_max.py
@@ -9,7 +9,10 @@ class TestDegree(TestCase):
         self.assertEqual(second_max([1, 2]), 1)
         self.assertEqual(second_max([2, 2]), 2)
         self.assertEqual(second_max([1, 2, 3]), 2)
+        self.assertEqual(second_max([5, 4, 3, 2]), 4)
         self.assertEqual(second_max([1, 5, 4, 3, 2, 5]), 5)
+        self.assertEqual(second_max([-1, -5, -4, -3, -2, -5]), -2)
+        self.assertEqual(second_max([-1, -2, 0, 5]), 0)
 
 
 if __name__ == '__main__':
