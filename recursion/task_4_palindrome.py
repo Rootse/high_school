adedeab9447e6ffbfa6c4a19cdc46cd170b02f17
yan.frolov <yan.frolov@osinit.com>
2023-09-11 17:40:21 +0300

    change default values

--- recursion/task_4_palindrome.py
+++ recursion/task_4_palindrome.py
@@ -1,9 +1,30 @@
-def is_palindrome(s: str, start: int = 0, end: int = -1) -> bool:
+def is_palindrome1(s: str) -> bool:
+    if not hasattr(is_palindrome1, '_idx'):
+        is_palindrome1._idx = 0
+
     s = s.replace(' ', '')
-    if start == len(s) // 2:
+
+    if is_palindrome1._idx >= len(s) // 2:
+        is_palindrome1._idx = 0
         return True
 
-    if s[start] != s[end]:
+    if s[is_palindrome1._idx] != s[len(s) - is_palindrome1._idx - 1]:
         return False
 
-    return is_palindrome(s, start + 1, end - 1)
+    is_palindrome1._idx += 1
+    return is_palindrome1(s)
+
+
+def is_palindrome2(s: str, idx: int = None) -> bool:
+    if idx is None:
+        idx = 0
+
+    s = s.replace(' ', '')
+
+    if idx >= len(s) // 2:
+        return True
+
+    if s[idx] != s[len(s) - idx - 1]:
+        return False
+
+    return is_palindrome2(s, idx + 1)
--- recursion/tests/test_4_palindrome.py
+++ recursion/tests/test_4_palindrome.py
@@ -1,18 +1,28 @@
 from unittest import TestCase, main
-from recursion.task_4_palindrome import is_palindrome
+from recursion.task_4_palindrome import is_palindrome1, is_palindrome2
 
 
 class TestDegree(TestCase):
 
     def test_is_palindrome_1(self):
-        self.assertTrue(is_palindrome(''))
-        self.assertTrue(is_palindrome('a'))
-        self.assertTrue(is_palindrome('лёша на полке клопа нашёл'))
-        self.assertTrue(is_palindrome('я арка края'))
-        self.assertFalse(is_palindrome('абв'))
-        self.assertFalse(is_palindrome('аббв'))
-        self.assertFalse(is_palindrome('абва'))
-        self.assertTrue(is_palindrome('аб ба'))
+        self.assertTrue(is_palindrome1(''))
+        self.assertTrue(is_palindrome1('a'))
+        self.assertTrue(is_palindrome1('лёша на полке клопа нашёл'))
+        self.assertTrue(is_palindrome1('я арка края'))
+        self.assertFalse(is_palindrome1('абв'))
+        self.assertFalse(is_palindrome1('аббв'))
+        self.assertFalse(is_palindrome1('абва'))
+        self.assertTrue(is_palindrome1('аб ба'))
+
+    def test_is_palindrome_2(self):
+        self.assertTrue(is_palindrome2(''))
+        self.assertTrue(is_palindrome2('a'))
+        self.assertTrue(is_palindrome2('лёша на полке клопа нашёл'))
+        self.assertTrue(is_palindrome2('я арка края'))
+        self.assertFalse(is_palindrome2('абв'))
+        self.assertFalse(is_palindrome2('аббв'))
+        self.assertFalse(is_palindrome2('абва'))
+        self.assertTrue(is_palindrome2('аб ба'))
 
 
 if __name__ == '__main__':
